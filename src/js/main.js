0e6a95d17a86fd20ff6c504ea7013eee8c067ba9
Hudson8811 <hudson88@yandex.ru>
2023-11-20 21:07:04 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@
 
 // Elements
 const tabsContent = document.querySelectorAll(".tabs__content"), 
-      tabBtns = document.querySelectorAll(".tabs__tab");
+      tabBtns = document.querySelectorAll(".tabs__tab"),
+      calcRes = document.querySelectorAll(".calc__res");
 
 // Functions
 const changeActiveElement = (num) => tabsContent.forEach((elem, i) => i == num ? elem.style.display = "block" : elem.style.display = "none");
@@ -18,9 +19,12 @@ tabBtns.forEach(item => {
 
 const calcBlockWrap = document.querySelectorAll(".calc__test_block_wrap"),
       resultBtn = document.querySelector(".calc__result"),
-      calcResItem = document.querySelectorAll(".calc__res_item");
+      calcResItem = document.querySelectorAll(".calc__res_item"),
+      calcResReturn = document.querySelectorAll(".calc__res_return");
 
-calcResItem.forEach(item => item.style.display = "none");
+calcResItem.forEach(item => item.style.right = "-100%");
+calcResItem.forEach(item => item.classList.remove('calc__res_item-active'));
+calcRes.forEach(item => item.style.zIndex = "-100");
     
 
 // const deleteClassItem = (item, className) => item.classList.remove(className);
@@ -34,7 +38,7 @@ function main(element = calcBlockWrap) {
         calcTestItem.forEach(item => {
             item.addEventListener("click", (e) => {
                 calcTestItem.forEach(item => item.classList.remove("calc__test_item-active"));
-                item.classList.add("calc__test_item-active")
+                item.classList.add("calc__test_item-active");
             });
         });
     
@@ -42,12 +46,19 @@ function main(element = calcBlockWrap) {
 } 
 main();
 
-
+let countA;
+let countB;
+let countC;
 resultBtn.addEventListener("click", function() {
+    
+    testBlock.forEach((item, i) => {
+        item.style.right = "100%";
+    });
+    countA = 0;
+    countB = 0;
+    countC = 0;
     const itemNums = document.querySelectorAll(".calc__test_item");
-    let countA = 0;
-    let countB = 0;
-    let countC = 0;
+    
     let bigCount = false;
     let bigCount1 = false;
 
@@ -65,51 +76,57 @@ resultBtn.addEventListener("click", function() {
         }
         
     });
-    calcResItem.forEach(item => item.style.display = "none");
+    calcResItem.forEach(item => item.style.right = "-100%");
+    calcResItem.forEach(item => item.classList.remove('calc__res_item-active'));
+    calcRes.forEach(item => item.style.zIndex = "1");
 
 
 
     if(countA > countB && countA > countC) {
-        calcResItem[0].style.display = "flex"
+        calcResItem[0].style.display = "flex";
+        calcResItem[0].classList.add("calc__res_item-active");
     }
     if(countA == countB && countA > countC) {
         bigCount = true;
-        calcResItem[0].style.display = "flex"
+        calcResItem[0].style.display = "flex";
+        calcResItem[0].classList.add("calc__res_item-active");
     }
     if(countA > countB && countA == countC) {
+        bigCount = true;
         if(!bigCount1) {
-            calcResItem[0].style.display = "flex"
+            calcResItem[0].style.display = "flex";
+            calcResItem[0].classList.add("calc__res_item-active");
         }
     }
 
 
 
     if(countB > countA && countB > countC) {
-        calcResItem[1].style.display = "flex"
+        calcResItem[1].classList.add("calc__res_item-active");
     }
     if(countB == countA && countB > countC) {
         if(!bigCount) {
-            calcResItem[1].style.display = "flex"
+        calcResItem[1].classList.add("calc__res_item-active");
         }
     }
     if(countB > countA && countB == countC) {
         bigCount1 = true;
-        calcResItem[1].style.display = "flex"
+            calcResItem[1].classList.add("calc__res_item-active");
     }
 
 
 
     if(countC > countA && countC > countB) {
-        calcResItem[2].style.display = "flex"
+        calcResItem[2].classList.add("calc__res_item-active");
     }
     if(countC == countA && countC > countB) {
         if(!bigCount) {
-            calcResItem[2].style.display = "flex"
+        calcResItem[2].classList.add("calc__res_item-active");
         }
     }
     if(countC > countA && countC == countB) {
         if(!bigCount1) {
-            calcResItem[2].style.display = "flex"
+            calcResItem[2].classList.add("calc__res_item-active");
         }
     }
 
@@ -129,3 +146,47 @@ for (let anchor of anchors) {
     })
   })
 }
+
+
+
+
+const testBlock = document.querySelectorAll(".calc__test_block");
+
+testBlock.forEach((item, i) => i == 0 ? item.style.right = "0" : item.style.right = "-100%");
+let right = 20;
+testBlock.forEach((item, i) => {
+    item.addEventListener("click", function() {
+        if(testBlock[i+1]) {
+            item.style.right = "100%";
+            item.style.scale = "0";
+            testBlock[i+1].style.right = `${right}%`;
+            testBlock[i+1].style.scale = "1";
+            right = right + 20;
+        } else {
+            resultBtn.style.display = "block";
+        }
+    });
+    
+});
+
+calcResReturn.forEach(item => {
+    item.addEventListener("click", function() {
+        countA = 0;
+        countB = 0;
+        countC = 0;
+        calcResItem.forEach(item => item.style.right = "-100%");
+        calcResItem.forEach(item => item.classList.remove('calc__res_item-active'));
+        calcRes.forEach(item => item.style.zIndex = "-100");
+        resultBtn.style.display = "none";
+        testBlock.forEach((item, i) => i == 0 ? item.style.right = "0" : item.style.right = "-100%");
+        testBlock.forEach((item, i) => {
+            if(i == 0) {
+                item.style.scale = "1";
+            } else {
+                item.style.scale = "0";
+            }
+        });
+        main();
+        right = 20;
+    });
+});
